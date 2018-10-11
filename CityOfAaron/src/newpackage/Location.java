1803452c1792044461446f280cb3a57aad230323
Rafael.M <domivene2009@hotmail.com>
2018-10-11 15:20:08 -0400

    Added the Location, Map, and Points Classes, I will add the attributes later.

---
+++ CityOfAaron/src/newpackage/Location.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+import java.io.Serializable;
+/**
+ *
+ * @author rmonzon
+ */
+public class Location implements Serializable{
+    // class variiable
+    private int    NameId;
+    private String address;
+    private String City; 
+    private String description;
+    
+    
+}
---
+++ CityOfAaron/src/newpackage/Point.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+import java.io.Serializable;
+/**
+ *
+ * @author rmonzon
+ */
+public class Point implements Serializable{
+    // Class variable 
+    private int rows;
+    private int columns; 
+    private int[] coordinates;
+    
+}
