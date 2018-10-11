aba4c648cca4cfd14180d2f896fe5d65bfc13fc6
Rafael.M <domivene2009@hotmail.com>
2018-10-11 15:07:22 -0400

    Added the Question Class with all variable and attributes

--- CityOfAaron/src/newpackage/Question.java
+++ CityOfAaron/src/newpackage/Question.java
@@ -4,11 +4,123 @@
  * and open the template in the editor.
  */
 package newpackage;
-
+import java.io.Serializable;
+import java.util.Objects;
 /**
  *
  * @author Rafael.M
  */
-public class Question {
+public class Question implements Serializable{
+    /// Class variable 
+    private String question;
+    private String answer1;
+    private String answer2;
+    private String answer3;
+    private String answer4;
+    private String answer5;
+
+    public Question() {
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer1() {
+        return answer1;
+    }
+
+    public void setAnswer1(String answer1) {
+        this.answer1 = answer1;
+    }
+
+    public String getAnswer2() {
+        return answer2;
+    }
+
+    public void setAnswer2(String answer2) {
+        this.answer2 = answer2;
+    }
+
+    public String getAnswer3() {
+        return answer3;
+    }
+
+    public void setAnswer3(String answer3) {
+        this.answer3 = answer3;
+    }
+
+    public String getAnswer4() {
+        return answer4;
+    }
+
+    public void setAnswer4(String answer4) {
+        this.answer4 = answer4;
+    }
+
+    public String getAnswer5() {
+        return answer5;
+    }
+
+    public void setAnswer5(String answer5) {
+        this.answer5 = answer5;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" + "question=" + question + ", answer1=" + answer1 + ", answer2=" + answer2 + ", answer3=" + answer3 + ", answer4=" + answer4 + ", answer5=" + answer5 + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 71 * hash + Objects.hashCode(this.question);
+        hash = 71 * hash + Objects.hashCode(this.answer1);
+        hash = 71 * hash + Objects.hashCode(this.answer2);
+        hash = 71 * hash + Objects.hashCode(this.answer3);
+        hash = 71 * hash + Objects.hashCode(this.answer4);
+        hash = 71 * hash + Objects.hashCode(this.answer5);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Question other = (Question) obj;
+        if (!Objects.equals(this.question, other.question)) {
+            return false;
+        }
+        if (!Objects.equals(this.answer1, other.answer1)) {
+            return false;
+        }
+        if (!Objects.equals(this.answer2, other.answer2)) {
+            return false;
+        }
+        if (!Objects.equals(this.answer3, other.answer3)) {
+            return false;
+        }
+        if (!Objects.equals(this.answer4, other.answer4)) {
+            return false;
+        }
+        if (!Objects.equals(this.answer5, other.answer5)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
     
 }
