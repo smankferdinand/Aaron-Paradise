c5fe725ff777fac26fd8da590fc5d76c91649ff1
rmonzon2017 <domivene2009@hotmail.com>
2018-10-29 22:06:27 -0400

    Added seven test methods from our matrix for ControlLayer class

--- CityOfAaron/test/cityofaaron/ControlLayerTest.java
+++ CityOfAaron/test/cityofaaron/ControlLayerTest.java
@@ -22,14 +22,129 @@ public class ControlLayerTest {
      */
     @Test
     public void testWheatRatEaten() {
+        
         System.out.println("wheatRatEaten");
-        int rnd = 5;
-        int wheat = 250;
-        int expResult = 0;
-        int result = ControlLayer.wheatRatEaten(rnd, wheat);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+     
+        /*
+        /////////////////////////////////////
+        TEST CASE # 1
+        /////////////////////////////////////
+        */
+        System.out.println("\tTes number 01");
+        //input for users 
+        int rnd = 50;
+        int wheat = 5500;
+        // create instance 
+      //  ControlLayer  newlayer = new ControlLayer();
+        // expected output 
+        int expResult = 440;
+        // call the function 
+        int result = ControlLayer.wheatRatEaten(rnd,wheat);
+        
+        
+        
+        /*
+        /////////////////////////////////////
+        TEST CASE # 2
+        /////////////////////////////////////
+        */
+        System.out.println("\tTes number 02");
+        //input for users 
+         rnd = 25;
+         wheat = 4700;
+        // create instance 
+     
+        // expected output 
+         expResult = 376;
+        // call the function 
+         result = ControlLayer.wheatRatEaten(rnd,wheat);
+        
+        
+        /*
+        /////////////////////////////////////
+        TEST CASE # 3
+        /////////////////////////////////////
+        */
+        System.out.println("\tTes number 03");
+        //input for users 
+         rnd = 60;
+         wheat = 3400;
+        // create instance 
+   
+        // expected output 
+         expResult = 272;
+        // call the function 
+         result = ControlLayer.wheatRatEaten(rnd,wheat);
+        
+        /*
+        /////////////////////////////////////
+        TEST CASE # 4
+        /////////////////////////////////////
+        */
+        System.out.println("\tTes number 04");
+        //input for users 
+         rnd = 30;
+         wheat = 2900;
+        // create instance 
+   
+        // expected output 
+         expResult = 232;
+        // call the function 
+         result = ControlLayer.wheatRatEaten(rnd,wheat); 
+         
+         
+           /*
+        /////////////////////////////////////
+        TEST CASE # 5
+        /////////////////////////////////////
+        */
+        System.out.println("\tTes number 05");
+        //input for users 
+         rnd = 4;
+         wheat = 8450;
+        // create instance 
+   
+        // expected output 
+         expResult = 676;
+        // call the function 
+         result = ControlLayer.wheatRatEaten(rnd,wheat); 
+      
+         
+         
+                /*
+        /////////////////////////////////////
+        TEST CASE # 6
+        /////////////////////////////////////
+        */
+        System.out.println("\tTes number 06");
+        //input for users 
+         rnd = 0;
+         wheat = -10;
+        // create instance 
+   
+        // expected output 
+         expResult = 0;
+        // call the function 
+         result = ControlLayer.wheatRatEaten(rnd,wheat); 
+         
+         
+         
+                  /*
+        /////////////////////////////////////
+        TEST CASE # 7
+        /////////////////////////////////////
+        */
+        System.out.println("\tTes number 07");
+        //input for users 
+         rnd = 0;
+         wheat = -45120;
+        // create instance 
+   
+        // expected output 
+         expResult = -3609;
+        // call the function 
+         result = ControlLayer.wheatRatEaten(rnd,wheat); 
     }
+    
     
 }
