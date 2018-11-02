bf32282ddc2aa0a6dbf2a4f34da6e84325e7c238
Ferdinand <smankferdinand1@gmail.com>
2018-11-02 01:20:37 +0100

    Update CityOfAaron/test/cityofaaron/BottleofWaterControlTest.java

--- CityOfAaron/test/cityofaaron/BottleofWaterControlTest.java
+++ CityOfAaron/test/cityofaaron/BottleofWaterControlTest.java
@@ -25,14 +25,128 @@ public class BottleofWaterControlTest {
     @Test
     public void testWatertaken() {
         System.out.println("watertaken");
-        int rnd = 5;
-        int water = 250;
-        int expResult = 0;
-        int result = BottleofWaterControl.watertaken(rnd, water);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        /*
+        /////////////////////////////////////
+        TEST CASE # 1
+        /////////////////////////////////////
+        */
+        System.out.println("\tTes number 01");
+        //input for users 
+        int rnd = 50;
+        int water = 5500;
+        // create instance 
+      //  BottleofWaterControl  newlayer = new BottleofWaterControl();
+        // expected output 
+        int expResult = 440;
+        // call the function 
+        int result = BottleofWaterControl.watertaken(rnd,water);
+        
+        
+        
+        /*
+        /////////////////////////////////////
+        TEST CASE # 2
+        /////////////////////////////////////
+        */
+        System.out.println("\tTes number 02");
+        //input for users 
+         rnd = 25;
+         water = 4700;
+        // create instance 
+     
+        // expected output 
+         expResult = 376;
+        // call the function 
+         result = BottleofWaterControl.watertaken(rnd,water);
+        
+        
+        /*
+        /////////////////////////////////////
+        TEST CASE # 3
+        /////////////////////////////////////
+        */
+        System.out.println("\tTes number 03");
+        //input for users 
+         rnd = 60;
+         water = 3400;
+        // create instance 
+   
+        // expected output 
+         expResult = 272;
+        // call the function 
+         result = BottleofWaterControl.watertaken(rnd,water);
+        
+        /*
+        /////////////////////////////////////
+        TEST CASE # 4
+        /////////////////////////////////////
+        */
+        System.out.println("\tTes number 04");
+        //input for users 
+         rnd = 30;
+         water = 2900;
+        // create instance 
+   
+        // expected output 
+         expResult = 232;
+        // call the function 
+         result = BottleofWaterControl.watertaken(rnd,water);
+         
+         
+           /*
+        /////////////////////////////////////
+        TEST CASE # 5
+        /////////////////////////////////////
+        */
+        System.out.println("\tTes number 05");
+        //input for users 
+         rnd = 4;
+         water = 8450;
+        // create instance 
+   
+        // expected output 
+         expResult = 676;
+        // call the function 
+         result = BottleofWaterControl.watertaken(rnd,water); 
+      
+         
+         
+                /*
+        /////////////////////////////////////
+        TEST CASE # 6
+        /////////////////////////////////////
+        */
+        System.out.println("\tTes number 06");
+        //input for users 
+         rnd = 0;
+         water = -10;
+        // create instance 
+   
+        // expected output 
+         expResult = 0;
+        // call the function 
+         result = BottleofWaterControl.watertaken(rnd,water); 
+         
+         
+         
+                  /*
+        /////////////////////////////////////
+        TEST CASE # 7
+        /////////////////////////////////////
+        */
+        System.out.println("\tTes number 07");
+        //input for users 
+         rnd = 0;
+         water = -45120;
+        // create instance 
+   
+        // expected output 
+         expResult = -3609;
+        // call the function 
+         result = BottleofWaterControl.watertaken(rnd,water); 
     }
+    
     
 }
 
